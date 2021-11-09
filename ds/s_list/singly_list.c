1c8a63dbee33216d0bdcb748658695fa27f129b1
almog.omer <almog2572@gmail.com>
2021-11-09 12:09:06 +0000

    new files

--- ds/s_list/singly_list.c
+++ ds/s_list/singly_list.c
@@ -15,6 +15,154 @@ struct SListNode {
 };
 
 
+/*typedef struct 
+{
+    size_t idNum;
+} person_t;
+
+
+static int is_match_func(const void* data, void* param)
+{
+    person_t* p = (person_t*)data;
+
+    if (p->idNum == *(size_t*)param)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+static int print(void* data, void* param)
+{
+
+	(void)param;
+	printf("%lu\n", *(size_t*)data);
+	return 1;
+}
+
+
+int main() 
+{
+
+    slist_t *list = SListCreate();
+    slist_t* list2 = SListCreate();
+	
+    
+	slist_iter_t iter = SListBegin(list);
+	slist_iter_t iter2 = SListBegin(list);
+	slist_iter_t iter3 = SListBegin(list2);
+	
+	int a = 5;
+	char b [15] = "abc";
+	int c = 100;
+	size_t test1;
+	person_t p1, p2, p3;
+    	slist_iter_t iter4, iter5, iter6;
+	
+	
+	if(!(SListIterIsEqual(iter, iter2) == 1))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+		
+	
+    	if(!(SListCount(list) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	SListInsert(iter, &a);
+	
+	if(!(SListCount(list) == 1))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	if(!(*(int*)SListIterGetData(iter) == 5))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	
+	SListInsert(iter, b); 
+	
+	
+	if(!(SListCount(list) == 2))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+
+	if(!(strcmp((char*)SListIterGetData(iter),"abc" ) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	SListInsert(iter, &c);
+	
+	if(!(SListCount(list) == 3))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+
+	if(!(*(int*)SListIterGetData(iter) == 100))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	
+	SListRemove(iter);
+	
+	if(!(SListCount(list) == 2))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+
+	SListDestroy(list);
+	
+	***********************************************************
+	
+	p1.idNum = 111;
+	p2.idNum = 445;
+	p3.idNum = 123;
+	
+	SListInsert(iter3, &p1);
+    	SListInsert(iter3, &p2);
+    	SListInsert(iter3, &p3);
+    	
+    	if(!(SListCount(list2) == 3))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+    	
+	iter4 = SListBegin(list2);
+	iter5 = SListEnd(list2);
+	
+	
+	if(!(SListIterIsEqual(iter4, iter5) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	
+	test1 = 445;
+	
+	iter6 = SListFind(SListBegin(list2), SListEnd(list2), is_match_func, &test1);
+	
+	if(!(((person_t*)SListIterGetData(iter6))->idNum == 445))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	
+	SListForEach(SListBegin(list2), SListEnd(list2), print, NULL);
+	
+	SListDestroy(list2);
+	
+   return 0;
+}*/
 
 /* Create new list */
 slist_t *SListCreate(void)
@@ -48,9 +196,11 @@ slist_t *SListCreate(void)
 /* Delete the list */
 void SListDestroy(slist_t *list)
 {
+	
 	snode_t *node_temp;
 	snode_t *temp;
 	node_temp = list->head;
+	assert(list);
 	
 	while(node_temp != NULL)
 	{	
@@ -59,6 +209,8 @@ void SListDestroy(slist_t *list)
 		node_temp = temp;
 		
 	}	
+	
+	free(node_temp);
 	free(list);
 
 }
@@ -67,7 +219,9 @@ void SListDestroy(slist_t *list)
 void SListInsert(slist_iter_t iter, const void *item)
 {
 	
+	
 	snode_t *new_node = (snode_t *) malloc(sizeof(snode_t));
+	assert(item && iter.node);
 	
 	if (NULL == new_node)
 	{
@@ -87,7 +241,11 @@ void SListInsert(slist_iter_t iter, const void *item)
 slist_iter_t SListBegin(slist_t *list)
 {
 	slist_iter_t begin_iter;
+	assert(list);
+	
 	begin_iter.node = list->head;
+	
+
 	return begin_iter;
 }
 
@@ -95,6 +253,7 @@ slist_iter_t SListEnd(slist_t *list)
 {
 	snode_t *temp = NULL;
 	slist_iter_t end_iter;
+	assert(list);
 	
 	temp = list->head;
 	while(temp->next != NULL)
@@ -109,12 +268,16 @@ slist_iter_t SListEnd(slist_t *list)
 
 int SListIterIsEqual(slist_iter_t iter1, slist_iter_t iter2)
 {
-	return (iter1.node->data == iter2.node->data);
+	assert(iter1.node && iter2.node);
+	return (iter1.node == iter2.node);
 }
 
 void SListRemove(slist_iter_t iter)
 {
 	snode_t *node;
+	assert(iter.node);
+	if (!iter.node->next)
+		return;
 	node = iter.node->next;
 	iter.node->data = iter.node->next->data;
 	iter.node->next = iter.node->next->next;
@@ -126,10 +289,9 @@ void SListRemove(slist_iter_t iter)
 size_t SListCount(slist_t *list)
 {
 	size_t counter = 0;
-	snode_t *node_temp;
-	
-	node_temp = list->head;
-	
+	snode_t *node_temp = list->head ;
+	assert(list);
+
 	while(node_temp->data != NULL)
 	{	
 		
@@ -144,17 +306,17 @@ size_t SListCount(slist_t *list)
 
 void* SListIterGetData(slist_iter_t iter)
 {
+	assert(iter.node);
 	return iter.node->data;
 }
 
 slist_iter_t SListIterNext(slist_iter_t iter)
 {
-	/*slist_iter_t iter_next;
 	
-	iter_next.node = */
-	
-	iter.node->data = iter.node->next->data;
-	iter.node->next = iter.node->next->next;
+	assert(iter.node);
+	if (!iter.node->next)
+		return iter;
+	iter.node = iter.node->next;
 	
 	return iter;
 }
@@ -165,6 +327,7 @@ slist_iter_t SListFind(slist_iter_t from, slist_iter_t to, match_func_t is_match
 	slist_iter_t match_iter;
 	
 	match_iter.node = NULL;
+	assert(from.node && to.node);
 	
 	while (!SListIterIsEqual(from, to))
 	{
@@ -183,16 +346,19 @@ slist_iter_t SListFind(slist_iter_t from, slist_iter_t to, match_func_t is_match
 
 int SListForEach(slist_iter_t from, slist_iter_t to, action_func_t action_func, void *param)
 {
-	int counter = 0;
+	
+	assert(from.node && to.node);
+	 
 	while (from.node != to.node)
 	{
-		action_func(from.node->data, param);
-		++counter;
-		
+		if(!action_func(from.node->data, param))
+		{
+			return 0;
+		}
 		from.node = from.node->next;
 	}
 	
-	return counter;
+	return 1;
 }
 
 
