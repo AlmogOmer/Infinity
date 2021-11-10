97b3a0f57ba19eaf8d0e082aa161c4d05a4d10d0
almog.omer <almog2572@gmail.com>
2021-11-10 09:06:58 +0000

    new files

--- ds/queue/queue.h
+++ ds/queue/queue.h
@@ -11,20 +11,22 @@ extern queue_t *QueueCreate(void);
 /*destroy queue*/
 extern void QueueDestroy(queue_t *queue);
 
-/*return to sizeof queue*/
-extern size_t QueueSize(const queue_t *queue);
-
-/*check if queue is empty*/
-extern int QueueIsEmpty(const queue_t *queue);	/* return value empty - 1, not empty - 0 */
-
-/* insert new item to back of the queue ,return SUCCESS /FAILURE */
-extern int QueueEnqueue(queue_t *queue, void *val); 
+/* insert new item to back of the queue */
+extern void QueueEnqueue(queue_t *queue, const void *val); 
 
 /* pop item from the front of the queue*/
 extern void QueueDequeue(queue_t *queue); 
 
 /*get value of the front item of the queue*/
 extern void *QueuePeek(const queue_t *queue); 
+
+/*return the sizeof queue*/
+extern size_t QueueSize(const queue_t *queue);
+
+/*check if queue is empty*/
+extern int QueueIsEmpty(const queue_t *queue);	/* return value empty - 1, not empty - 0 */
+
+
 
 
 #endif	
