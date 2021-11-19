4e407d1609e9e1f895966af5690d7a0c920471c3
almog.omer <almog2572@gmail.com>
2021-11-19 15:08:46 +0000

    new files

---
+++ ds/scheduler/scheduler.c
@@ -0,0 +1,189 @@
+#include "scheduler.h"
+#include <stdlib.h>	/* for malloc */
+#include <assert.h>	/* for assertions */
+#include <unistd.h>	/* for sleep */
+#include <time.h>	/* for time */
+#include "../uid/uid.h"
+#include "../pri_queue/priority_queue.h"
+#include "../sorted_list/sorted_list.h"
+
+struct scheduler
+{
+	pri_queue_t *task_queue;
+
+};
+struct task 
+{
+	unique_id_t uid;
+	task_func_t task_func;
+	void *params;
+	time_t interval_in_secs;
+	time_t execute_time;
+};
+
+task_t *TaskCreate(task_func_t task_func, void *params, time_t interval_in_secs)
+{
+	task_t *task = NULL;
+	
+	
+	task = (task_t *) malloc(sizeof(task_t));
+	if (!task)
+	{
+		return NULL;
+	}
+	
+	task->uid = UIDGenerate();
+	task->task_func = task_func;
+	task->params = params;
+	task->interval_in_secs = interval_in_secs;
+	task->execute_time = time(NULL) + interval_in_secs;
+	
+	return task;
+}
+
+time_t TaskGetExecTime(const task_t *task)
+{
+	
+	return (task->execute_time);
+}
+
+void TaskUpdateExecTime(task_t *task)
+{
+	
+	task->execute_time += task->interval_in_secs;
+}
+
+/* return value- 0 abort and 1 for continue looping */
+int TaskExecute(task_t *task)
+{
+	assert(task);
+	
+	return (task->task_func(task->params));
+}
+
+
+static int IsPrior(const void *data1, const void *data2, const void *param)
+{
+	assert(data1 && data2);
+	(void)param;
+	
+	return (TaskGetExecTime(data1) <= TaskGetExecTime(data2));
+}
+
+
+/*creates new scheduler engine*/
+scheduler_t *SchedulerCreate()
+{
+	scheduler_t *sch = (scheduler_t *) malloc(sizeof(scheduler_t));
+	if (!sch)
+	{
+		return NULL;
+	}
+	
+	sch->task_queue = PriQueueCreate(IsPrior, NULL);
+	if (!sch->task_queue)
+	{
+		free(sch);
+		return NULL;
+	}
+	
+	
+	return sch;
+
+}
+
+/*destroy scheduler*/
+void SchedulerDestroy(scheduler_t *scheduler)
+{
+	/* free all nodes and tasks stored in them */
+	while (!SchedulerIsEmpty(scheduler))
+	{
+		free(PriQueuePeek(scheduler->task_queue));	
+	}
+	
+	/* free the empty queue */
+	PriQueueDestroy(scheduler->task_queue);
+	
+	/* free the control block */
+	free(scheduler);
+
+
+}
+
+/* insert new task to the scheduler engine*/
+unique_id_t SchedulerTaskAdd(scheduler_t *scheduler, task_func_t task, 
+						size_t interval_in_secs, void *param)
+{
+	task_t *added_task = NULL;
+	
+	added_task = TaskCreate(task, param, interval_in_secs);
+	
+	PriQueueEnqueue(scheduler->task_queue, added_task);
+	
+	return (added_task->uid);
+
+}
+
+/* remove task from scheduler engine*/
+extern void SchedulerTaskCancel(scheduler_t *scheduler, unique_id_t uid);
+
+/* run the scheduler engine */
+int SchedulerRun(scheduler_t *scheduler)
+{
+	task_t *next_task = NULL;
+	
+	assert(scheduler);
+	assert(!SchedulerIsEmpty(scheduler));
+	
+	while (1)
+	{
+		if (SchedulerIsEmpty(scheduler))
+		{
+			return 0;
+		}
+		
+		next_task = PriQueuePeek(scheduler->task_queue);
+		PriQueueDequeue(scheduler->task_queue);
+		
+		sleep(TaskGetExecTime(next_task) - time(NULL));
+		
+		if (next_task->task_func(next_task->params))		/* task continues */
+		{
+			TaskUpdateExecTime(next_task);
+			
+		}
+		else
+		{
+			free(next_task);
+		}
+	}
+	
+	return 1;
+
+
+}
+
+/* stop running scheduler */
+extern void SchedulerStop(scheduler_t *scheduler);
+
+/* return the size of the scheduler */
+size_t SchedulerSize(scheduler_t *scheduler)
+{
+	assert(scheduler);
+	
+	return PriQueueSize(scheduler->task_queue);
+}
+
+/*check if scheduler is empty*/
+int SchedulerIsEmpty(scheduler_t *scheduler)
+{
+	assert(scheduler);
+	
+	return PriQueueIsEmpty(scheduler->task_queue);
+
+}
+
+/*stop the scheduler*/
+extern scheduler_t *SchedulerClear(scheduler_t *scheduler);
+
+
---
+++ ds/scheduler/scheduler_test.c
@@ -0,0 +1,113 @@
+#include "scheduler.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+
+static int task0(const void *param);
+static int task1(const void *param);
+static int task2(const void *param);
+static int task3(const void *param);
+static int task_stop(const void *param);
+
+int main()
+{
+	
+	scheduler_t *scheduler = SchedulerCreate();
+	unique_id_t uid1 = uid_null_uid;
+	if(!(SchedulerSize(scheduler) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	if(!(SchedulerIsEmpty(scheduler) == 1))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	scheduler = scheduler;
+	SchedulerTaskAdd(scheduler, task1, 5, "Dumbeldore");
+	SchedulerTaskAdd(scheduler, task2, 2, "Voldemort");
+	SchedulerTaskAdd(scheduler, task3, 7, "Harry Potter");
+	
+	if(!(SchedulerSize(scheduler) == 3))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	if(!(SchedulerIsEmpty(scheduler) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	uid1 = SchedulerTaskAdd(scheduler, task0, 1, "a Death Eater");
+	/*SchedulerTaskCancel(scheduler, uid1);
+	
+	if(!(SchedulerSize(scheduler) == 3))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	SchedulerTaskAdd(scheduler, task_stop, 15, scheduler);
+	
+	SchedulerClear(scheduler);
+	
+	if(!(SchedulerSize(scheduler) == 0))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	if(!(SchedulerIsEmpty(scheduler) == 1))
+	{
+		printf("fail in %d\n", __LINE__);
+	}
+	
+	SchedulerTaskAdd(scheduler, task1, 5, "Dumbeldore");
+	SchedulerTaskAdd(scheduler, task2, 2, "Voldemort");
+	SchedulerTaskAdd(scheduler, task3, 7, "Harry Potter");
+	SchedulerTaskAdd(scheduler, task_stop, 15, scheduler);
+	uid1 = SchedulerTaskAdd(scheduler, task0, 1, "a Death Eater");
+	SchedulerTaskCancel(scheduler, uid1);
+	
+	if(!(SchedulerSize(scheduler) == 4))
+	{
+		printf("fail in %d\n", __LINE__);
+	}*/
+	
+	SchedulerRun(scheduler);
+	
+	SchedulerDestroy(scheduler);
+	return 0;
+}
+
+static int task1(const void *param)
+{
+	printf("hello i am task1, %s\n", (char*)param);
+	return 1;
+}
+
+static int task2(const void *param)
+{
+	printf("hello i am task2, %s\n", (char*)param);
+	return 1;
+}
+
+static int task3(const void *param)
+{
+	printf("hello i am task3, %s\n", (char*)param);
+	return 1;
+}
+
+static int task0(const void *param)
+{
+	printf("hello i am task0, %s\n", (char*)param);
+	return 1;
+}
+
+/*static int task_stop(const void *param)
+{
+	SchedulerStop((scheduler_t*)param);
+	return 0;
+}*/
+
