d1ad77cb26a59c317a85516beb93efb9cceaf571
almog.omer <almog2572@gmail.com>
2022-01-17 14:18:36 +0200

    shell

---
+++ system_programming/simple_shell/shell.c
@@ -0,0 +1,54 @@
+#include <unistd.h>		/* for fork, exec.. */
+#include <stdlib.h>		/* for EXIT_SUCCESS */
+#include <sys/wait.h>	/* for wait */
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	pid_t pid;
+	int child_status;
+	size_t i;
+	char *argv[50];
+	char *token = NULL;
+	const char s[2] = " ";
+	char *buf = (char *)calloc(15,sizeof(char));
+
+	while (1)
+	{
+		i = 0;
+		scanf("%[^\n]%*c", buf);
+    	
+		token = strtok(buf, s);
+
+		while(token != NULL)
+		{
+			argv[i] = token;
+			token = strtok(NULL, s);
+			++i;
+		}
+		
+		if (0 == strcmp("exit", argv[0]))
+		{
+			exit(1);
+		}
+		
+		pid = fork();
+
+		if (0 == pid)
+		{
+			execvp(argv[0], argv);
+		}
+		
+		else
+		{
+			child_status = 0;
+			wait(&child_status);
+		}
+	}
+
+	return 0;
+}
+
+
