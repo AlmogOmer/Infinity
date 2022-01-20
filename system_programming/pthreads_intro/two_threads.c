1ca0a014a247a70bd5f162ced50bfdffcadf2998
almog.omer <almog2572@gmail.com>
2022-01-20 15:56:25 +0200

    pthreads

--- system_programming/pthreads_intro/two_threads.c
+++ system_programming/pthreads_intro/two_threads.c
@@ -3,15 +3,16 @@
 #include <unistd.h>			/* for sleep		*/
 #include <stdio.h>		
 
-static void *Print1(void *arg);
-static void *Print2(void *arg);
+static void *Print(void *arg);
 
 int main(void)
 {
 	static pthread_t thread_id[2] = {0};
+    char *arg1 = "Im thread 1";
+    char *arg2 = "Im thread 2";
 
-	pthread_create(thread_id , NULL, Print1, NULL);
-    pthread_create(thread_id + 1, NULL, Print2, NULL);
+	pthread_create(thread_id , NULL, Print, arg1);
+    pthread_create(thread_id + 1, NULL, Print, arg2);
 
     pthread_join(*(thread_id), NULL);
     pthread_join(*(thread_id + 1), NULL);
@@ -19,14 +20,12 @@ int main(void)
 	return 0;
 }
 
-static void *Print1(void *arg)
+static void *Print(void *arg)
 {
     size_t i = 0;
-    (void)arg;
-
     while(i<30)
     {
-        printf("Im thread 1\n");
+        printf("%s\n", (char *)arg);
         sleep (2);
         ++i;
     }
@@ -34,18 +33,5 @@ static void *Print1(void *arg)
     return NULL;
 }
 
-static void *Print2(void *arg)
-{
-	size_t i = 0;
-    (void)arg;
 
-    while(i<30)
-    {
-        printf("Im thread 2\n");
-        sleep (2);
-        ++i;
-    }
-
-	return NULL;
-}
 
