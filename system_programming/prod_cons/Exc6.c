83e8f7c45cf9cc20e5b3919bb02754f741639dbe
almog.omer <almog2572@gmail.com>
2022-01-30 18:59:55 +0200

    update

--- system_programming/prod_cons/Exc6.c
+++ system_programming/prod_cons/Exc6.c
@@ -58,17 +58,17 @@ static void *Producer(void *param)
     exc6_t *exc6 = (exc6_t *) param;
 
 	while (1)
-	{		
+	{
+		for (i = 0; i < NUM_OF_THREADS; ++i)
+        {
+            sem_wait(&exc6->semaphore);
+        }
+		
 		pthread_mutex_lock(&exc6->lock);
 		exc6->write = rand() % 50;
         pthread_mutex_unlock(&exc6->lock);
         
         pthread_cond_broadcast(&exc6->cond);
-
-		for (i = 0; i < NUM_OF_THREADS; ++i)
-        {
-            sem_wait(&exc6->semaphore);
-        }
 
 		printf("producer write is %lu\n", exc6->write);
 
@@ -85,12 +85,14 @@ static void *Consumer(void *param)
 	while (1)
 	{
 		size_t value = 0;
+		sem_post(&exc6->semaphore);
 		
         pthread_mutex_lock(&exc6->lock);
         pthread_cond_wait(&exc6->cond,&exc6->lock); 
 		value = exc6->write;
+		pthread_mutex_unlock(&exc6->lock);
 		
-		sem_post(&exc6->semaphore);
+
 		printf("consumer read is %lu\n", value);
 
 	}
