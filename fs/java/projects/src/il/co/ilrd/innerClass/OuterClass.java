a8bb6d6d2e889ba11fa400e8382f89ba943cbdcd
almog.omer <almog2572@gmail.com>
2022-02-17 15:48:59 +0200

    innerclass

--- fs/java/projects/src/il/co/ilrd/innerClass/OuterClass.java
+++ fs/java/projects/src/il/co/ilrd/innerClass/OuterClass.java
@@ -1,22 +1,46 @@
 package il.co.ilrd.innerClass;
 
+interface Age {
+    int x = 21;
+    void getAge();
+}
+
 public class OuterClass {
     int myouter;
     public OuterClass(int myouter){
         this.myouter = myouter;
     }
     
-    public void PrintMe(int myouter){
+    public void PrintMe(){
         System.out.println(myouter);
     } 
 
     public class innerClass{
         
-        //OuterClass.PrintMe(myouter);
-   
+        public void test(){
+            OuterClass.this.PrintMe();
+            OuterClass.this.myouter = 9;
+            OuterClass.this.PrintMe();
+        }
+
+        public void testAnonymous(){
+            Age oj1 = new Age() {
+           
+                @Override public void getAge()
+                {
+                    System.out.print("Age is " + x);
+                }
+            };
+           
+            oj1.getAge();
+        }
     }
 
     public static void main(String[] args) {
+        innerClass obj = new OuterClass(6).new innerClass();
+        obj.test();
+        obj.testAnonymous();
+
     }
         
         
