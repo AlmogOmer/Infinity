8d911454c4839cab4b46bad174de51d2380a1959
almog.omer <almog2572@gmail.com>
2022-03-17 17:11:05 +0200

    update

--- fs/java/projects/src/il/co/ilrd/executor/ExecutorExercise.java
+++ fs/java/projects/src/il/co/ilrd/executor/ExecutorExercise.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ExecutorExercise {
@@ -33,139 +34,33 @@ public class ExecutorExercise {
 
     public static void SinglePool() {
         ExecutorService e1 = Executors.newSingleThreadExecutor();
-        List<Callable<String>> callables = new ArrayList<Callable<String>>();
-        List<Future<String>> futures = new ArrayList<>();
-
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 1";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 2";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 3";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 4";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 5";
-            }
-        });
-
-        long start = System.nanoTime(); 
-        
-        try {
-            futures = e1.invokeAll(callables);
-        } catch (InterruptedException e2) {
-            e2.printStackTrace();
-        }
-
-        for(Future<String> future : futures){
-            try {
-                System.out.println("future.get = " + future.get());
-            } catch (InterruptedException | ExecutionException e) {
-
-                e.printStackTrace();
-            }
-        }
-        
-        e1.shutdown();
-        long end = System.nanoTime(); 
-        System.out.println("SinglePool : " + (end-start));
+        Test(e1,  "SinglePool : ");
     }
 
     public static void FixedPool() {
         ExecutorService e1 = Executors.newFixedThreadPool(3);
-        List<Callable<String>> callables = new ArrayList<Callable<String>>();
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 1";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 2";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 3";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 4";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 5";
-            }
-        });
-
-        long start = System.nanoTime(); 
-
-        List<Future<String>> futures = null;
-        try {
-            futures = e1.invokeAll(callables);
-        } catch (InterruptedException e2) {
-            e2.printStackTrace();
-        }
-
-        for(Future<String> future : futures){
-            try {
-                System.out.println("future.get = " + future.get());
-            } catch (InterruptedException | ExecutionException e) {
-
-                e.printStackTrace();
-            }
-        }
-        
-        e1.shutdown();
-        long end = System.nanoTime(); 
-        System.out.println("FixedPool : " + (end-start));
+        Test(e1,  "FixedPool : ");
     }
 
     public static void CachedPool() {
         ExecutorService e1 = Executors.newCachedThreadPool();
+        Test(e1,  "CachedPool : ");
+    }
+
+    public static void Test(ExecutorService e1,  String name){
         List<Callable<String>> callables = new ArrayList<Callable<String>>();
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 1";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 2";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 3";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 4";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 5";
-            }
-        });
+        List<Future<String>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 5; ++i){
+            callables.add(new Callable<String>() {
+                public String call() throws Exception {
+                    return "Task";
+                }
+            });
+        }
+        
         long start = System.nanoTime(); 
-        List<Future<String>> futures = null;
+        
         try {
             futures = e1.invokeAll(callables);
         } catch (InterruptedException e2) {
@@ -183,38 +78,20 @@ public class ExecutorExercise {
         
         e1.shutdown();
         long end = System.nanoTime(); 
-        System.out.println("CachedPool : " + (end-start));
+        System.out.println(name + (end-start));
     }
-
+    
     public static void SchedualedPool() {
         ScheduledExecutorService e1 = Executors.newSingleThreadScheduledExecutor();
         List<Callable<String>> callables = new ArrayList<Callable<String>>();
         List<Future<String>> futures = new ArrayList<>();
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 1";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 2";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 3";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 4";
-            }
-        });
-        callables.add(new Callable<String>() {
-            public String call() throws Exception {
-                return "Task 5";
-            }
-        });
+        for (int i = 0; i < 5; ++i){
+            callables.add(new Callable<String>() {
+                public String call() throws Exception {
+                    return "Task";
+                }
+            });
+        }
         long start = System.nanoTime(); 
         
         for(Callable<String> call : callables){
@@ -236,6 +113,25 @@ public class ExecutorExercise {
         System.out.println("SchedualedPool : " + (end-start));
     }
 
+    public static void SchedualedPool1() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        Runnable beeper = new Runnable() {
+            public void run() { System.out.println("beep"); }
+          };
+        
+        ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 1, 1, TimeUnit.SECONDS);
+
+        /*scheduler.schedule(new Runnable() {
+            public void run() { beeperHandle.cancel(true); }}, 10, TimeUnit.SECONDS);*/
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        scheduler.shutdown();
+    }
 
 
     public static void main(String[] args) {
@@ -244,6 +140,7 @@ public class ExecutorExercise {
         FixedPool();
         CachedPool();
         SchedualedPool();
+        SchedualedPool1();
 
     }
 
